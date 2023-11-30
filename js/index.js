8cdea8949ddc36a5c7c63ad75db02c514e26166d
Natsuki Ueno <135255999+nueno13@users.noreply.github.com>
2023-12-01 00:36:45 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,41 @@
+//drawer
+$(function(){
+    $('.drawer').drawer();
+});
+
+
+//smoothscroll
+// #から始まるURLがクリックされた時
+
+$('a[href^="#"]').click(function () {
+  //移動速度を指定
+  let speed = 300;
+  //hrefで指定されたidを取得
+  let id = $(this).attr("href");
+  //idの値が#のみだったらターゲットをhtmlにしてトップに戻るようにする
+  let target = $("#" == id ? "html" : id);
+  //ページのトップを基準にターゲットの位置を取得
+  let position = $(target).offset().top;
+  //ターゲットの位置までspeedの速度で移動
+
+  $("html, body").animate(
+    {
+      scrollTop: position - $("#js-header").outerHeight()
+    },
+    speed,
+    "swing" //swing or linear
+  ); 
+  return false;
+});
+  
+
+//wow.js
+
+new WOW().init();
+
+
+
+
+
+
+  
